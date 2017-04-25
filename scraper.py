7847fdc2bd7acb70831b97da6240d44e364e4f0e
Piotr Kilczuk <piotr@tymaszweb.pl>
2017-04-25 16:39:29 +0100

    Parse train parameters

--- scraper.py
+++ scraper.py
@@ -1,11 +1,16 @@
 import datetime
 
+from bs4 import BeautifulSoup
 import requests
 
 
 LOCATIONS = [
     'COLTONJ',
 ]
+
+
+def log(message):
+    print(message)
 
 
 def main():
@@ -15,7 +20,35 @@ def main():
             location=location, today=datetime.date.today(),
         )
         response = requests.get(url)
-        print(response)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        trains = soup.find_all('tr')
+
+        log('Found {count} trains for {location}'.format(count=len(trains), location=location))
+
+        for train in trains:
+            columns = train.find_all('td')
+            if not columns:
+                continue
+
+            column_values = [c.string for c in columns]
+            arrival = column_values[1]
+            origin = column_values[3]
+            headcode = column_values[5]
+            toc = column_values[6]
+            destination = column_values[7]
+            departure = column_values[8]
+
+
+            print('arr', arrival)
+            print('ori', origin)
+            print('headcode', headcode)
+            print('toc', toc)
+            print('dest', destination)
+            print('dep', departure)
+
+
+            raise NotImplementedError
 
 
 if __name__ == '__main__':
