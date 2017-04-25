cc5a99d919b1ec2d411c46287fc927a71d0391b8
Piotr Kilczuk <piotr@tymaszweb.pl>
2017-04-25 17:02:21 +0100

    Introduce is_interesting

--- scraper.py
+++ scraper.py
@@ -4,8 +4,17 @@ from bs4 import BeautifulSoup
 import requests
 
 
+# http://www.railwaycodes.org.uk/crs/CRS0.shtm
 LOCATIONS = [
     'COLTONJ',
+]
+
+INTERESTING_STATIONS = [
+    'Doncaster West Yard',
+    'Holbeck Loco Sidings',
+    'Royal Mail',
+    'Shields T.M.D.',
+    'York Thrall Europa',
 ]
 
 
@@ -13,8 +22,18 @@ def log(message):
     print(message)
 
 
+def is_interesting(train_params):
+    for station in INTERESTING_STATIONS:
+        if station in train_params['origin'] or station in train_params['destination']:
+            return True
+
+
 def main():
+    interesting = {}
+
     for location in LOCATIONS:
+        interesting[location] = []
+
         url = 'http://www.realtimetrains.co.uk/search/advanced/{location}/' \
               '{today.year}/{today.month:02d}/{today.day:02d}/0000-2359?stp=WVS&show=all&order=wtt'.format(
             location=location, today=datetime.date.today(),
@@ -32,23 +51,20 @@ def main():
                 continue
 
             column_values = [c.string for c in columns]
-            arrival = column_values[1]
-            origin = column_values[3]
-            headcode = column_values[5]
-            toc = column_values[6]
-            destination = column_values[7]
-            departure = column_values[8]
+            train_params = {
+                'arrival': column_values[1],
+                'origin': column_values[3],
+                'headcode': column_values[5],
+                'toc': column_values[6],
+                'destination': column_values[7],
+                'departure': column_values[8],
+            }
 
+            if is_interesting(train_params):
+                interesting[location].append(train_params)
 
-            print('arr', arrival)
-            print('ori', origin)
-            print('headcode', headcode)
-            print('toc', toc)
-            print('dest', destination)
-            print('dep', departure)
-
-
-            raise NotImplementedError
+    import pprint
+    pprint.pprint(interesting)
 
 
 if __name__ == '__main__':
